c4e930b67a18c9f1a68bcd6f017ae055c74ed950
pj-nx <197416111+pj-nx@users.noreply.github.com>
2025-10-22 15:56:36 +0530

    Add more error logs

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1310,13 +1310,35 @@ class AccountMoveLine(models.Model):
     @api.constrains('account_id', 'tax_ids', 'tax_line_id', 'reconciled')
     def _check_off_balance(self):
         for line in self.move_id.line_ids:
-            if line.account_id.internal_group == 'off_balance':
-                if any(a.internal_group != line.account_id.internal_group for a in line.move_id.line_ids.account_id):
-                    raise UserError(_('If you want to use "Off-Balance Sheet" accounts, all the accounts of the journal entry must be of this type'))
-                if line.tax_ids or line.tax_line_id:
-                    raise UserError(_('You cannot use taxes on lines with an Off-Balance account'))
-                if line.reconciled:
-                    raise UserError(_('Lines from "Off-Balance Sheet" accounts cannot be reconciled'))
+            try:
+                if line.account_id.internal_group == "off_balance":
+                    if any(
+                        a.internal_group != line.account_id.internal_group
+                        for a in line.move_id.line_ids.account_id
+                    ):
+                        raise UserError(
+                            _(
+                                'If you want to use "Off-Balance Sheet" accounts, all the accounts of the journal entry must be of this type'
+                            )
+                        )
+                    if line.tax_ids or line.tax_line_id:
+                        raise UserError(
+                            _(
+                                "You cannot use taxes on lines with an Off-Balance account"
+                            )
+                        )
+                    if line.reconciled:
+                        raise UserError(
+                            _(
+                                'Lines from "Off-Balance Sheet" accounts cannot be reconciled'
+                            )
+                        )
+
+            except Exception as e:
+                _logger.error(
+                    f"Error during off-balance sheet check for line {line} ({line.account_id}, {line.account_id.internal_group}): {e}"
+                )
+                raise
 
     @api.constrains('account_id', 'display_type')
     def _check_payable_receivable(self):
