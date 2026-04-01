5a6ec267223cc1469ad7264cbc823eb89da3d78f
pj-nx <197416111+pj-nx@users.noreply.github.com>
2026-04-01 23:54:13 +0530

    Fix permission error for login

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -1202,7 +1202,7 @@ class Website(models.Model):
 
     @api.model
     def is_public_user(self):
-        return request.env.user.id == request.website._get_cached('user_id')
+        return request.env.user and request.env.user.id and request.env.user.id == request.website._get_cached('user_id')
 
     @api.model
     def viewref(self, view_id, raise_if_not_found=True):
